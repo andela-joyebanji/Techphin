f651edea65fc1d9ce629422a4e1b03fdf4afd4d3
andela-joyebanji <oyebanji.jacob@andela.com>
2016-04-30 00:07:48 +0100

    [Feature #118678395] Force SSL on Production
    - Add condition to force ssl in `AppServiceProvider.php`
    - Write test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /**
+         * This forces links to https in production.
+         *
+         * Reference: http://www.jeffmould.com/2016/01/31/laravel-5-2-forcing-https-routes-when-using-ssl/
+         */
+        if (!\App::environment('local')) {
+              \URL::forceSchema('https');
+         }
     }
 
     /**
